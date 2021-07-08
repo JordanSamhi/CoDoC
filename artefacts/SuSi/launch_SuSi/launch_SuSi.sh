a963d73ddeada7c60785307d388c1e3bcd2152e5
Jordan Samhi <j.samhi@me.com>
2021-07-08 14:32:06 +0200

    Add: SuSi launcher

---
+++ artefacts/SuSi/launch_SuSi/launch_SuSi.sh
@@ -0,0 +1,1 @@
+java -cp lib/slf4j-api-1.7.9.jar:lib/slf4j-simple-1.7.9.jar:lib/slf4j-ext-1.7.9.jar:lib/slf4j-log4j12-1.7.9.jar:lib/weka.jar:lib/soot-trunk.jar:lib/soot-infoflow.jar:lib/soot-infoflow-android.jar:SuSi.jar de.ecspride.sourcesinkfinder.SourceSinkFinder android-30.jar permissionMethodWithLabel.pscout out.pscout
