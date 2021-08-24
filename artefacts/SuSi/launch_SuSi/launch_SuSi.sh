5925ec3776aae08757d7354c4884b0417e2a3364
Jordan Samhi <jordan.samhi@uni.lu>
2021-08-24 11:17:30 +0200

    Add: options

--- artefacts/SuSi/launch_SuSi/launch_SuSi.sh
+++ artefacts/SuSi/launch_SuSi/launch_SuSi.sh
@@ -1,1 +1,15 @@
-java -cp lib/slf4j-api-1.7.9.jar:lib/slf4j-simple-1.7.9.jar:lib/slf4j-ext-1.7.9.jar:lib/slf4j-log4j12-1.7.9.jar:lib/weka.jar:lib/soot-trunk.jar:lib/soot-infoflow.jar:lib/soot-infoflow-android.jar:SuSi.jar de.ecspride.sourcesinkfinder.SourceSinkFinder android-30.jar permissionMethodWithLabel.pscout out.pscout
+while getopts f: option
+do
+    case "${option}"
+        in
+        f) PSCOUT_FILE_PATH=${OPTARG};;
+    esac
+done
+
+if [ -z "$PSCOUT_FILE_PATH" ]
+then
+    echo
+    read -p "Pscout file path: " PSCOUT_FILE_PATH
+fi
+
+java -cp lib/slf4j-api-1.7.9.jar:lib/slf4j-simple-1.7.9.jar:lib/slf4j-ext-1.7.9.jar:lib/slf4j-log4j12-1.7.9.jar:lib/weka.jar:lib/soot-trunk.jar:lib/soot-infoflow.jar:lib/soot-infoflow-android.jar:SuSi.jar de.ecspride.sourcesinkfinder.SourceSinkFinder android-30.jar $PSCOUT_FILE_PATH out.pscout
